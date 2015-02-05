250dc9daba5aeca4f80deabc154496c796c43baa
Jean de Klerk <jadekler@gmail.com>
2015-02-05 01:07:03 +0000

    Cathode with multiple light ups

--- cathode.py
+++ cathode.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+import sys
 
 GPIO.setmode(GPIO.BCM)
 
@@ -7,20 +8,34 @@ blue_pin = 5
 green_pin = 6
 red_pin = 13
 
-GPIO.setup(green_pin, GPIO.OUT)
-GPIO.setup(red_pin, GPIO.OUT)
-GPIO.setup(blue_pin, GPIO.OUT)
+uptime = .2
 
-def lightup(pin, uptime):
-    GPIO.output(pin, 1)
+pins = [blue_pin, green_pin, red_pin]
+
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
+
+def lightup(pins, uptime):
+    for pin in pins:
+        GPIO.output(pin, 1)
+    
     time.sleep(uptime)
-    GPIO.output(pin, 0)
+    
+    for pin in pins:
+        GPIO.output(pin, 0)
+    
     time.sleep(uptime)
 
 try:
-    lightup(blue_pin, .2)
-    lightup(green_pin, .2)
-    lightup(red_pin, .2)
+    # Algorithms...how do they even work
+    for pin1 in pins:
+        lightup([pin1], uptime)
+        for pin2 in pins:
+            lightup([pin1, pin2], uptime)
+            for pin3 in pins:
+                lightup([pin1, pin2, pin3], uptime)
+        
 except:
-    print "Unexpected error: ", sys.exc_info()[0]
+    print sys.exc_info()
+
 GPIO.cleanup()
