c6b200b8fad97d4706a1392f922f5af3a0b2091e
Jean de Klerk <jadekler@gmail.com>
2015-02-05 19:27:09 -0700

    Working recursive permutations (finally..), but has duplicates

--- common.py
+++ common.py
@@ -1,27 +1,17 @@
 class Common:
     def permutations(self, arr):
-        return self.permutations_foo(arr, 0, 3)
+        return self.permutations_foo(arr, [], 0, 1)
 
-    def permutations_foo(self, arr, start, dist):
+    def permutations_foo(self, arr, prefix, start, dist):
         permutations = []
 
-        print "i j d l res"
+        for i in range(start, len(arr)):
+            if dist == 1:
+                permutations += [prefix+[arr[i]]]
+            else:
+                permutations += self.permutations_foo(arr, prefix+[arr[i]], i+1, dist-1)
 
-        for i in range(len(arr)):
-            for j in range(len(arr)-i-dist+1):
-                j += i+dist-1
-
-                res = arr[i:i+dist-1]+[arr[j]]
-
-                print i, j, dist, len(arr), res
-
-                if i+dist <= len(arr):
-                    permutations.append(res)
-
-        # if (start < len(arr)):
-        #     permutations.append(self.permutations_foo(arr, start+1, dist))
-
-        # if (dist < len(arr)):
-        #     permutations += self.permutations_foo(arr, start, dist+1)
-
-        return permutations
+        if dist == len(arr):
+            return permutations
+        else:
+            return permutations + self.permutations_foo(arr, prefix, start, dist+1)
