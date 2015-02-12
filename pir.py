12d0ae3ae67647c760a59dc9f8c4eed13bf786a0
Jean de Klerk <jadekler@gmail.com>
2015-02-12 19:49:47 +0000

    Added pir sensing program. To be expanded out to ping rest api about status.

---
+++ pir.py
@@ -0,0 +1,33 @@
+import time
+import RPi.GPIO as io
+import atexit
+
+io.setmode(io.BCM)
+
+pir_pin = 18
+green_pin = 17
+red_pin = 27
+
+io.setup(pir_pin, io.IN)
+io.setup(green_pin, io.OUT)
+io.setup(red_pin, io.OUT)
+
+def exit_handler():
+    print "SHUTTING DOWN"
+    io.output(green_pin, 0)
+    io.output(red_pin, 0)
+
+atexit.register(exit_handler)
+
+while True:
+    io.output(green_pin, 0)
+    io.output(red_pin, 0)
+
+    if io.input(pir_pin):
+        print("PIR ALARM")
+        io.output(green_pin, 1)
+    else:
+        print("NADA")
+        io.output(red_pin, 1)
+
+    time.sleep(0.5)
