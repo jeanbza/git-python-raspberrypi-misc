b915a7479892962f1fc912ffc0fcc779c0039847
Jean de Klerk <jadekler@gmail.com>
2015-02-12 18:34:06 -0700

    Moved PIR into its own folder. Added post request to PIR.

---
+++ pir/README.md
@@ -0,0 +1,6 @@
+# PIR sensor that pings activity to remote API
+---
+
+### Installation
+
+1. pip install -r requirements.txt
--- pir/pir.py
+++ pir/pir.py
@@ -1,6 +1,7 @@
 import time
 import RPi.GPIO as io
 import atexit
+import requests
 
 io.setmode(io.BCM)
 
@@ -25,9 +26,11 @@ while True:
 
     if io.input(pir_pin):
         print("PIR ALARM")
+        requests.post("pipingpong.cfapps.io/activity", data={'active': 1})
         io.output(green_pin, 1)
     else:
         print("NADA")
+        requests.post("pipingpong.cfapps.io/activity", data={'active': 0})
         io.output(red_pin, 1)
 
     time.sleep(0.5)
